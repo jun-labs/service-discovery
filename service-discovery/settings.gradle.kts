2a0ce6611fe52a226a4bbcf4a6528865ef23d03b
devjun10 <devjun1023@gmail.com>
2023-09-29 20:23:31 +0900

    chore: project build
    - 프로젝트 빌드
     *SpringCloud, Eureka 의존성 추가

---
+++ service-discovery/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+            }
+        }
+    }
+}
+include(
+    "service-discovery",
+    "order-api",
+    "product-api-1",
+    "product-api-2",
+    "user-api"
+)
+rootProject.name = "service-discovery"
