08c9c1839f6c0e14e044d8ee3eafaac60c9eae7b
devjun10 <devjun1023@gmail.com>
2023-10-04 18:51:48 +0900

    feature: add codeandmessage

---
+++ rate-limiter/support/src/main/java/project/gateway/product/common/codeandmessage/ErrorCodeAndMessage.java
@@ -0,0 +1,8 @@
+package project.gateway.product.common.codeandmessage;
+
+public interface ErrorCodeAndMessage extends CodeAndMessage{
+
+    String getKrErrorMessage();
+
+    String getEnErrorMessage();
+}
