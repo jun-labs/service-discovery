6ccc1849f6c9f5f4bed7fdd20b1b42d94c702bb8
devjun10 <devjun1023@gmail.com>
2023-11-19 17:43:56 +0900

    feature: add route configuration

---
+++ forwarding/api-gateway/src/main/kotlin/project/springcloud/gateway/common/configuration/router/RouteConfiguration.kt
@@ -0,0 +1,23 @@
+package project.springcloud.gateway.common.configuration.router
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RouteConfiguration(
+    @Value("\${location.product}")
+    private val productServerUrl: String
+) {
+    @Bean
+    fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        return builder.routes()
+            .route { router ->
+                router.path("/**")
+                    .uri(productServerUrl)
+            }
+            .build()
+    }
+}
